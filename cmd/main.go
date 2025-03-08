c9552929f8b947b5a0c1dd7a233e933454360151
kremls <kremlsa@yandex.ru>
2025-03-08 16:54:31 +0700

    First commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/kremlsa/gtf-homework/config"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/kremlsa/gtf-homework/configs"
 	"github.com/kremlsa/gtf-homework/internal/pages"
+	slogfiber "github.com/samber/slog-fiber"
 )
 
 func main() {
+
+	// Создаём логгер
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	app := fiber.New()
-	appConf := config.NewAppConfig()
-	pages.PageHandler(app)
+
+	// Добавляем конфигурацию приложения
+	appConf := configs.NewAppConfig()
+
+	// Подключаем логгер
+	app.Use(slogfiber.New(logger))
+	app.Use(recover.New())
+
+	pages.PageHandler(app, logger)
 
 	app.Listen(fmt.Sprintf(":%d", appConf.Port))
 }
--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -1,11 +1,16 @@
 package pages
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log/slog"
 
-func PageHandler(router fiber.Router) {
+	"github.com/gofiber/fiber/v2"
+)
+
+func PageHandler(router fiber.Router, logger *slog.Logger) {
 	router.Get("/", firstApi)
 }
 
 func firstApi(c *fiber.Ctx) error {
+
 	return c.SendString("response")
 }
